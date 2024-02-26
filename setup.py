048664079c7b0ec59520267ab31db49e561a3dfa
mahegz <hegzmahmoud@gmail.com>
2024-02-26 11:04:35 +0100

    refactor

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from setuptools import setup
 from Cython.Build import cythonize
 import numpy
 
-setup(ext_modules=cythonize("utils_fast.pyx"), include_dirs=[numpy.get_include()])
+setup(ext_modules=cythonize("utils/utils_fast.pyx"), include_dirs=[numpy.get_include()])
