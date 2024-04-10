6b0bfd37ab330d5e693237f85991b3ce6a815ae4
InesVATI <ines.vati@eleves.enpc.fr>
2024-04-10 18:13:49 +1000

    code release

--- start.py
+++ start.py
@@ -26,16 +26,15 @@ if __name__ == "__main__":
     out_test = myCKN(Xte)
     out_test = out_test.reshape(out_test.shape[0], -1)
 
-    data_mean = jnp.load(os.path.join(models_folder, 'scaler_features_means.npy'))
-    data_std = jnp.load(os.path.join(models_folder, 'scaler_features_std.npy'))
+    # data_mean = jnp.load(os.path.join(models_folder, 'scaler_features_means.npy'))
+    # data_std = jnp.load(os.path.join(models_folder, 'scaler_features_std.npy'))
 
-    X = (out_test - data_mean)/data_std
+    X = (out_test - out_test.mean(axis=0, keepdims=True))/out_test.std(axis=0, keepdims=True)
     print(f'X mean {X.mean(axis=0)} X var {X.var(axis=0)}')
-
 
     kernel_func = RBF(sigma=jnp.sqrt(X.shape[1]))
     my_svm = MultiClassKernelSVM(num_classes=10, kernel_func=kernel_func, c=1)
-    with open(f'{models_folder}/svm_full.pkl', 'rb') as f:
+    with open(f'{models_folder}/classifier_full.pkl', 'rb') as f:
         my_svm = pickle.load(f)
 
     Yte = my_svm.predict(X)
